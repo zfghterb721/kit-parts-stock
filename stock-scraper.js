d77f008df6b19741b0f612d62adc176a85698a2c
Glitch (hello-sqlite) <none>
2020-03-01 22:42:18 +0000

    ⛄️👣 Checkpoint ./stock-scraper.js:3749127/751

--- stock-scraper.js
+++ stock-scraper.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const https = require('https');
 
 //This function grabs stock from tayda electronics based on the page name.
 //everything on tayde is in the format https://www.taydaelectronics.com/${itemName}.html
@@ -53,9 +54,26 @@ async function getAlliedStock(productNumber) {
 async function getNewarkStock(productNumber) {
   console.log("here",productNumber);
   try {
-    const webpage = await axios.get(`https://www.newark.com/${productNumber}`);
+    https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) => {
+  let data = '';
+
+  // A chunk of data has been recieved.
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+    console.log(JSON.parse(data).explanation);
+  });
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});
+    const webpage = await https.get(`https://www.newark.com/${productNumber}`);
+    console.log(webpage)
     return parseInt(
-      webpage.data.match(/    ([0-9\,]+) In stock/)[1].replace(",", "")
+      webpage.match(/class="availabilityHeading available">\s{8}([0-9\,]+) In stock/)[1].replace(",", "")
     );
   } catch (e) {
     console.log(e);
